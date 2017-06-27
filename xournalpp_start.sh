bd8b24a36b0a5a40a3a19399d34571e1a6be03c6
Jesse Stevenson <jessedusty@gmail.com>
2017-06-27 11:31:59 -0400

    Update xournalpp_start.sh

--- xournalpp_start.sh
+++ xournalpp_start.sh
@@ -1,3 +1,14 @@
-# Starts xournalpp in a docker contianer using the $DISPLAY env variable and volume mounts $HOME
 xhost +
-docker run -it --rm -e DISPLAY=$DISPLAY -u `id -u` -e HOME=$HOME -v $HOME:$HOME -v /tmp/.X11-unix:/tmp/.X11-unix jessedusty/xournalpp
+
+#docker run -it --rm -e DISPLAY=$DISPLAY -v /root:/root -v /tmp/.X11-unix:/tmp/.X11-unix --entrypoint "bash" jessedusty/xournalpp 
+tag=latest
+
+if [ $# -ne 0 ]; then
+	docker run -i --rm -e DISPLAY=$DISPLAY -u `id -u` \
+		-e HOME=$HOME -v $HOME:$HOME \
+		-v /tmp/.X11-unix:/tmp/.X11-unix jessedusty/xournalpp:$tag "$*"
+else
+	docker run -i --rm -e DISPLAY=$DISPLAY -u `id -u` \
+		-e HOME=$HOME -v $HOME:$HOME \
+		-v /tmp/.X11-unix:/tmp/.X11-unix jessedusty/xournalpp:$tag
+fi
